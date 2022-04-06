015fec87b2bbfa2b7ba87bc5a7dce04e91e68965
CAndRyan <CAndRyan@users.noreply.github.com>
2022-04-05 21:45:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hypotenuse
+
+A suite of tools to manage component rendering & data storage.
+Uses JSX syntax & select principles from React & Redux.
---
+++ webpack.config.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'mtbjorn-hypotenuse.js',
+        library: 'hypotenuse',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+      },
+    resolve: {
+        extensions: ['', '.js', '.mjs'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
